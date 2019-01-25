3123096bd65c2c23f39e313d4c4c461cab619a5f
jenniferlai43 <jenniferlai43@gmail.com>
2019-01-24 18:18:26 -0800

    rendering html instead of displaying text

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('hello.html')
