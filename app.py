f7bde95f00b2e935b4527fbded3238e0a53162fd
jenniferlai43 <jenniferlai43@gmail.com>
2019-02-13 01:39:42 -0800

    made minimal changes to frontend

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__, static_url_path='', static_folder='static') #/static folde
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 POSTGRES = {
-    'user': 'when2meet',
+    'user': 'postgres',
     'pw': '1234',
     'db': 'when2meet_dev',
     'host': 'localhost',
