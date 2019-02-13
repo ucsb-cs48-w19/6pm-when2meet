24185ccc407db8ff0ff96efaa45ecd116408fcb0
rafaeltrinidad <35505147+rafaeltrinidad@users.noreply.github.com>
2019-02-12 18:29:36 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
+
 # 6pm-when2meet
+
+## Project summary
+
+### One-sentence description of the project
+
+When-2-meet is a webapp to help groups find an optimal time to meet.
+
+### Additional information about the project
+
+The web app is a convenient way for people to setup the perfect meeting time. The UI/UX is intuitive and our algorithm takes into account even small gaps of availability to provide the user with the optimal time to meet.
+
+## Installation
+
+### Prerequisites
+Python Flask
+PostgreSQL
+Pip
+
+### Installation Steps
+- Install pip/Python (pip comes with Python 2 >=2.7.9 or Python 3 >=3.4)
+- Setup a virtual environment in your project folder and install dependencies
+  - Make sure you have installed virtualenv on your computer: ```pip install virtualenv```
+  - Check virtualenv version with: ```virtualenv --version```
+  =Run the following:
+	```bash
+	$ cd my_project_folder
+	$ virtualenv venv
+	$ source venv/bin/activate
+	$ pip install -r requirements.txt #to install dependencies
+	```
+- Database setup instructions - PSQL (coming soon!)
+	- Install Postgres: ```brew install postgres``` on Mac on download it through website on Windows.
+	- Install Python's ORM for PSQL: ```pip install Flask-SQLAlchemy```
+	- Install Python database adapter: ```pip install psycopg2```
+	- Run postgres with: ```psql -U postgres``` or ```psql```
+		- Run following commands to create test database:
+			```psql
+			CREATE USER when2meet WITH PASSWORD '1234';
+			CREATE DATABASE when2meet_dev;
+			```
+	- On the terminal, run the following commands to do database migrations:
+		```
+		python manage.py db init #run this line once to intialize
+		python manage.py db migrate
+		python manage.py db upgrade
+		```
+
+
+## Functionality
+
+- Meeting creator gos to the website.
+- Meeting creator setups an event with a range of dates and a set period of time.
+- Meeting creator shares the link with all the meeting attendees.
+- The meeting attendees enter in their availablilities.
+- Once all attendees have entered in their availabilities, the meeting creator will be given an optimal meeting time.
+
+## Known Problems
+
+- TBD
+
+
+## Contributing
+
+TODO: Leave the steps below if you want others to contribute to your project.
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## License
+
+If you haven't already, add a file called `LICENSE.txt` with the text of the appropriate license.
+We recommend using the MIT license: <https://choosealicense.com/licenses/mit/>
+
+The Value Exchange: users are happy with the result
+
+Heroku Link: https://stark-headland-53397.herokuapp.com/
+=======
---
+++ app.py
@@ -0,0 +1,113 @@
+from flask import Flask
+from flask import redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+from models import Events, Users, TimeRanges
+from dotenv import load_dotenv
+import os
+import datetime
+import random, string
+
+
+load_dotenv()
+
+app = Flask(__name__, static_url_path='', static_folder='static') #/static folder to hold static files by default.
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': '1234',
+    'db': 'when2meet_dev',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+if DATABASE_URL is not None:
+	app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+	
+
+db.init_app(app)
+
+print('sql config: ', app.config['SQLALCHEMY_DATABASE_URI'])
+# print('DATABASE_URL: ', DATABASE_URL)
+# print('POSTGRES: ', POSTGRES)
+
+# test
+# def testCreateEvent():
+# 	tb = 2
+# 	ds = datetime.datetime.now()
+# 	de = datetime.datetime.now()
+# 	t = 'ABCD'
+# 	e = Events(timeblock = tb, dateStart = ds, dateEnd = de, token = t) 
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/events/<event_token>', methods=['GET', 'POST'])
+def event(event_token):
+    # show the post with the given id, the id is an integer
+    if request.method=='GET':
+    	print('in get')
+    	e = db.session.query(Events).filter(Events.token==event_token).first()
+    	dateS = e.dateStart.strftime('%m/%d/%Y')
+    	dateE = e.dateEnd.strftime('%m/%d/%Y')
+    	print(e)
+    	if e is None:
+    		return render_template('404.html')
+    	else:
+    		return render_template('event.html', event=e, dateS=dateS, dateE=dateE)
+    if request.method=='POST':
+    	print('in post')
+    	e = db.session.query(Events).filter(Events.token==event_token).first()
+    	print(request.form)
+    	username=request.form['username']
+    	print(username)
+    	print(e)
+    	u = Users(name=username, event=e)
+    	print('printing u', u)
+    	db.session.add(u)
+    	start_time=request.form['start_time']
+    	end_time=request.form['end_time']
+    	t = TimeRanges(user=u, timeStart=start_time, timeEnd=end_time)
+    	db.session.add(t)
+    	db.session.commit()
+    	return redirect('/')
+    #return 'Post %d' % post_id
+
+@app.route('/events/<event_token>/getTime', methods=['GET'])
+def get_time(event_token):
+	if request.method=='GET':
+		e = db.session.query(Events).filter(Events.token==event_token)
+
+
+@app.route('/create_event', methods=['GET','POST'])
+def create_event():
+	print('in-create')
+	if request.method=='GET':
+		return render_template('create.html')
+	if request.method=='POST':
+		print('in post')
+		print(request.form)
+		event_name=request.form['event_name']
+		start_date=request.form['start_date']
+		end_date=start_date
+		timeblock=request.form['timeblock']
+		#10 Digit/Char long Alphanumeric token generated randomly 
+		token= ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+		e = Events(name=event_name, timeblock=timeblock, dateStart=start_date, dateEnd=end_date, token=token)
+		db.session.add(e)
+		db.session.commit()
+		return render_template('token.html', token=token)
+
+
+
+	
+
+
+
