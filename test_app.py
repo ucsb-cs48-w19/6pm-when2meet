8bda1b60f23573b44f135bb318bbe195d2ad2c3e
jelanilewis97 <43737512+jelanilewis97@users.noreply.github.com>
2019-02-28 06:32:06 -0800

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,7 @@
+from project.models import User
+
+def test_event(event_token):
+    u = User(username="Jelani", event="First test")
+    assert username = "Jelani"
+    assert event ="First test"
+
