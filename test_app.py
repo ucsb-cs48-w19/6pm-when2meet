e3e93d463c1f7bfb5049aab0c9a536fff42e4bf1
jelanilewis97 <43737512+jelanilewis97@users.noreply.github.com>
2019-02-28 18:23:06 -0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,23 @@
-from project.models import User
+import pytest
+from project.models import Events, User
+import random
+import datetime
+ 
+ 
+@pytest.fixture(scope='module')
+def new_event():
+    event_name = "First Test"
+    start_date = datetime.datetime.now()
+    end_date = start_date
+    ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    timeblock = 1
+    
+    e = Events(name=event_name, timeblock=timeblock, dateStart=start_date, dateEnd=end_date, token=token)
+    
+    return e
 
-def test_event(event_token):
-    u = User(username="Jelani", event="First test")
-    assert username = "Jelani"
-    assert event ="First test"
-
+def test_create_event(new_event):
+    
+    
+    assert e.name == "First Test"
+    assert isinstance(e.dateStart,datetime.datetime)
