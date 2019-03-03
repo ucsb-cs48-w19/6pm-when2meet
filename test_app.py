e58793d32fd374839b82bf860d6b06f0af10f5d4
Vir Bedi <43729852+virbedi@users.noreply.github.com>
2019-03-02 17:16:46 -0800

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,28 @@
+import pytest
+from flask import Flask
+from flask import redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+from models import Events, Users, TimeRanges
+from dotenv import load_dotenv
+import os
+import datetime
+import random, string
+import psycopg2
+ 
+ 
+@pytest.fixture(scope='module')
+def new_event():
+    event_name = "First Test"
+    start_date = datetime.datetime.now()
+    end_date = start_date
+    token = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    timeblock = 1
+    
+    e = Events(name=event_name, timeblock=timeblock, dateStart=start_date, dateEnd=end_date, token=token)
+    
+    return e
+
+def test_create_event(new_event):    
+    assert new_event.name == "First Test"
+    assert isinstance(new_event.dateStart,datetime.datetime)
