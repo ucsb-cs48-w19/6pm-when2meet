118bdfcc06b25527a944c0b5a270afb91a8b0535
jelanilewis97 <43737512+jelanilewis97@users.noreply.github.com>
2019-03-03 19:21:14 -0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -41,8 +41,6 @@ def new_event2():
     end_date = start_date
     e = Events(name=event_name, timeblock=timeblock, dateStart=start_date, dateEnd=end_date, token=token)
     
-    
-    #return render_template('event.html', event=e, dateS=dateS, dateE=dateE)
     return e
    
 def test_event2(new_event2):
