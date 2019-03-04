91c32e8ba94edb23c3c01d05eb64cd52383e861c
Vir Bedi <43729852+virbedi@users.noreply.github.com>
2019-03-03 17:11:30 -0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -49,6 +49,6 @@ def new_event2():
     
     return render_template('event.html', event=e, dateS=dateS, dateE=dateE)
 
-def test_event2():
+def test_event2(new_event2):
     assert new_event2.name == "Second Test"
     assert isinstance(new_event2.dateStart,datetime.datetime)
