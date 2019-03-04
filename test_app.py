f07e6e459323b586610ce0e1ee9f7ea7d5631b4d
jelanilewis97 <43737512+jelanilewis97@users.noreply.github.com>
2019-03-04 02:45:27 -0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -10,7 +10,20 @@ import datetime
 import random, string
 import psycopg2
 
-from project import app, db
+class BaseTestCase(TestCase):
+
+    def create_app(self):
+        app.config.from_object('config.TestConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.add(new_event)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 
  
