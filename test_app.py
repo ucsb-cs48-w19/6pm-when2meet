61b8f5ce1559457cc1f4ea0cac4b0ed0505a6942
jelanilewis97 <43737512+jelanilewis97@users.noreply.github.com>
2019-03-06 02:19:54 -0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -61,13 +61,13 @@ class BaseTestCase(unittest.TestCase):
 class FlaskTestCases(BaseTestCase):
     #test to make sure if visiting broken link 404 is thrown
     def test_invalid_link(self):
-        app.get('/events/faketoken')
+        self.app.get('/events/faketoken')
         self.assert_template_used('hello.html')
 
     #test first event where optimal time should be 12pm-1pm
     def test_get_time(self):
         with app.test_request_context('events/easy10curl'):
-            response = app.get_time("easy10curl").data
+            response = get_time("easy10curl").data
             self.assertEquals("3/24/2019 12:00 PM to 3/24/2019 1:00 PM", response)
     
     
