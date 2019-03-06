818c946e4a5bae07cd254f327940d5f82367f68b
jelanilewis97 <43737512+jelanilewis97@users.noreply.github.com>
2019-03-05 16:25:57 -0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -65,8 +65,9 @@ class FlaskTestCases(BaseTestCase):
 
     #test first event where optimal time should be 12pm-1pm
     def test_get_time(self):
-        response = get_time("easy10curl")
-        self.assertEquals("3/24/2019 12:00 PM to 3/24/2019 1:00 PM", response.data)
+        with app.test_request_context('events/easy10curl'):
+            response = get_time("easy10curl")
+            self.assertEquals("3/24/2019 12:00 PM to 3/24/2019 1:00 PM", response.data)
     
     
     '''
